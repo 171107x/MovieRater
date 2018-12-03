8f74a8805da89887913429548d1b4877699cc9c5
171107x <171107x@mymail.nyp.edu.sg>
2018-12-03 22:37:18 +0800

    f

--- MovieRaterIntermidiate/app/src/main/java/com/example/user/movieraterintermidiate/MainActivity3.kt
+++ MovieRaterIntermidiate/app/src/main/java/com/example/user/movieraterintermidiate/MainActivity3.kt
@@ -1,8 +1,10 @@
 package com.example.user.movieraterintermidiate
 
+import com.example.user.movieraterintermidiate.MovieDetail
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+
 
 class MainActivity3 : AppCompatActivity() {
 
@@ -28,18 +30,5 @@ class MainActivity3 : AppCompatActivity() {
         language.text = moviedesc.language
         age.text = moviedesc.age
     }
-    class MovieDetail (movie : String, description : String, language: String, date: String, age: String) {
-        var movie: String
-        var description: String
-        var language: String
-        var date: String
-        var age: String
-        init {
-            this.movie = movie
-            this.description = description
-            this.language = language
-            this.date = date
-            this.age = age
-        }
-    }
+
 }
---
+++ MovieRaterIntermidiate/app/src/main/java/com/example/user/movieraterintermidiate/MovieDetail.kt
@@ -0,0 +1,16 @@
+package com.example.user.movieraterintermidiate
+
+class MovieDetail(movie : String, description : String, language: String, date: String, age: String) {
+    var movie: String
+    var description: String
+    var language: String
+    var date: String
+    var age: String
+    init {
+        this.movie = movie
+        this.description = description
+        this.language = language
+        this.date = date
+        this.age = age
+    }
+}
